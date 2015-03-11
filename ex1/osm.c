0128df09ccae80a7bdd31b057765d19b789f50ca
Gregory Pasternak <griffonn@gmail.com>
2015-03-11 10:38:14 +0200

    final polishes to ex1

--- ex1/osm.c
+++ ex1/osm.c
@@ -1,3 +1,19 @@
+/**
+ * @file osm.c
+ * @author  griffonn ednussi
+ * @version 1.0
+ * @date 10 Mar 2015
+ * 
+ * @brief Library for measuring different basic operations time
+ *
+ * @section LICENSE
+ * This program is a free software. You can freely redistribute it.
+ *
+ * @section DESCRIPTION
+ * This library provides functionality for measures time of simple arithmetic 
+ * operation, empty system call, and empty function call, and calculates ratios 
+ * between them.
+ */
 #include "osm.h"
 #include <sys/time.h>
 #include <unistd.h>
@@ -5,36 +21,21 @@
 #include <stdio.h>
 
 #define DEFAULT_ITERATION_NUMBER 50000
-#define DEFAULT_ITER_OPERATIONS_NUMBER 5
+#define DEFAULT_ITER_OPERATIONS_NUMBER 10
 #define MEGA pow(10, 3)
 #define GIGA pow(10, 6)
-#define TERA pow(10, 9)
-
 #define DEFAULT_BAD_RESULT -1
-
 #define WORKING_PROPERLY 0
-
-
 
 int main()
 {	
-	timeMeasurmentStructure tms = measureTimes(0);
-	printf("Hostname:\t\t\t%s\n", tms.machineName);
-	printf("Number of iterations:\t\t%d\n", tms.numberOfIterations);
-
-	printf("Function:\t\t\t%f\n", tms.functionTimeNanoSecond);
-	printf("Trap:\t\t\t\t%f\n", tms.trapTimeNanoSecond);
-	printf("Instruction\t\t\t%f\n", tms.instructionTimeNanoSecond);
-	
-	printf("Function/Instruction Ratio:\t%f\n", tms.functionInstructionRatio);
-	printf("Trap/Instruction Ratio:\t\t%f\n", tms.trapInstructionRatio);
-
 	return WORKING_PROPERLY;
 }
 
-/* Initialization function that the user must call
+/**
+ * Initialization function that the user must call
  * before running any other library function.
- * Returns 0 uppon success and -1 on failure.
+ * Returns 0 as does nothing, to be consistent with the API.
  */
 int osm_init()
 {
@@ -42,26 +43,30 @@ int osm_init()
 }
 
 /**
- *  @brief Calculates difference between two time points given in timespec format,
- *  	then measures average time of iteration for given iteration number.
- *  @param startTime a timespec struct with starting time
- *  @param stopTime a timespec struct with stop time
+ *  @brief Calculates difference between two time points given in timeval 
+ *		format, then measures average time of iteration for given iteration 
+ *		number.
+ *  @param startTime a timeval struct with starting time
+ *  @param stopTime a timeval struct with stop time
  *  @param osm_iterations number of iterations
  *  @return average time of one iteration in nanoseconds
  */
-// double calculateTimeDifference(struct timespec startTime, struct timespec stopTime)
-double calculateTimeDifference(struct timeval startTime, struct timeval stopTime)
+double calculateTimeDifference(struct timeval startTime, 
+							   struct timeval stopTime)
 {
 	double sec_diff = stopTime.tv_sec - startTime.tv_sec;
 	double msec_diff = stopTime.tv_usec - startTime.tv_usec;
 
-	double ms = sec_diff * GIGA + msec_diff;
-
-	// We need only nanoseconds
-	return MEGA * ms;
+	// We need in nanoseconds
+	return MEGA * (sec_diff * GIGA + msec_diff);
 }
 
-double averageGettingTimeTime(unsigned int iterations)
+/**
+ *  @brief Calculates average time of double gettimeofday call.
+ *  @param iterations number of iterations
+ *  @return average time of one iteration in nanoseconds
+ */
+double avgGettimeofdayCallTime(unsigned int iterations)
 {
 	struct timeval startTime;
 	struct timeval stopTime;
@@ -82,7 +87,12 @@ double averageGettingTimeTime(unsigned int iterations)
 	return calculateTimeDifference(startTime, stopTime) / iterations;
 }
 
-double averageForLoopRuntime(unsigned int iterations)
+/**
+ *  @brief Calculates average time of incrementing a loop counter variable.
+ *  @param iterations number of iterations
+ *  @return average time of one iteration in nanoseconds
+ */
+double avgForLoopRuntime(unsigned int iterations)
 {
 	struct timeval startTime;
 	struct timeval stopTime;
@@ -94,10 +104,7 @@ double averageForLoopRuntime(unsigned int iterations)
 		return DEFAULT_BAD_RESULT;
 	}
 
-	for (i = 0; i < iterations; i+=10)
-	{
-
-	}
+	for (i = 0; i < iterations; i += DEFAULT_ITER_OPERATIONS_NUMBER);
 
 	if (gettimeofday(&stopTime, NULL))
 	{
@@ -110,7 +117,7 @@ double averageForLoopRuntime(unsigned int iterations)
 /**
  *  @brief Empty function
  */
-void empty()
+void emptyFunc()
 {
 
 }
@@ -125,7 +132,25 @@ int isValidIterationsNumber(unsigned int osm_iterations)
 	return (osm_iterations != 0 && osm_iterations < INT_MAX);
 }
 
-/*  @brief Time measurement function for an empty function call.
+/** 
+ *  @brief Rounds up the osm_interations number to fit unrolling defaults.
+ *  @param osm_iterations number of iterations
+ *  @return 0 if the osm_iteration is valid
+ */
+int normalizeIterationsNumber(unsigned int osm_iterations)
+{
+	int remaider = osm_iterations % DEFAULT_ITER_OPERATIONS_NUMBER;
+
+	if (remaider != 0)
+	{
+		osm_iterations += DEFAULT_ITER_OPERATIONS_NUMBER - remaider;
+	}
+
+	return osm_iterations;
+}
+
+/**
+ *  @brief Time measurement function for an empty function call.
  *  	returns time in nano-seconds upon success, 
  *  	and -1 upon failure.
  *  	Zero iterations number is invalid.
@@ -140,46 +165,41 @@ double osm_function_time(unsigned int osm_iterations)
 		osm_iterations = DEFAULT_ITERATION_NUMBER;
 	}
 
-	// struct timespec startTime;
-	// struct timespec stopTime;
 	struct timeval startTime;
 	struct timeval stopTime;
+	unsigned int i;
 
 	// Start time measuring
-
-	unsigned int i;
-	
-	// if (clock_gettime(CLOCK_REALTIME, &startTime))
 	if (gettimeofday(&startTime, NULL))
 	{
 		return DEFAULT_BAD_RESULT;
 	}
 	
-	for (i = 0; i < osm_iterations; i+=10)
+	for (i = 0; i < osm_iterations; i += DEFAULT_ITER_OPERATIONS_NUMBER)
 	{
-		empty();
-		empty();
-		empty();
-		empty();
-		empty();
-		empty();
-		empty();
-		empty();
-		empty();
-		empty();
+		emptyFunc();
+		emptyFunc();
+		emptyFunc();
+		emptyFunc();
+		emptyFunc();
+
+		emptyFunc();
+		emptyFunc();
+		emptyFunc();
+		emptyFunc();
+		emptyFunc();
 	}
 
-	// if (clock_gettime(CLOCK_REALTIME, &stopTime))
 	if (gettimeofday(&stopTime, NULL))
 	{
 		return DEFAULT_BAD_RESULT;
 	}
 	
-	// Return difference
 	return calculateTimeDifference(startTime, stopTime) / osm_iterations;
 }
 
-/*  @brief Time measurement function for an empty trap into the operating system.
+/**
+ *  @brief Time measurement function for an empty trap into the OS.
  *  	returns time in nano-seconds upon success, 
  *  	and -1 upon failure.
  *  	Zero iterations number is invalid.
@@ -194,28 +214,24 @@ double osm_syscall_time(unsigned int osm_iterations)
 		osm_iterations = DEFAULT_ITERATION_NUMBER;
 	}
 
-	// struct timespec startTime;
-	// struct timespec stopTime;
 	struct timeval startTime;
 	struct timeval stopTime;
-
-	// Start time measuring
-
 	unsigned int i;
 
-	// if (clock_gettime(CLOCK_REALTIME, &startTime))
+	// Start time measuring
 	if (gettimeofday(&startTime, NULL))
 	{
 		return DEFAULT_BAD_RESULT;
 	}
 
-	for (i = 0; i < osm_iterations; i+=10)
+	for (i = 0; i < osm_iterations; i += DEFAULT_ITER_OPERATIONS_NUMBER)
 	{
 		OSM_NULLSYSCALL;
 		OSM_NULLSYSCALL;
 		OSM_NULLSYSCALL;
 		OSM_NULLSYSCALL;
 		OSM_NULLSYSCALL;
+
 		OSM_NULLSYSCALL;
 		OSM_NULLSYSCALL;
 		OSM_NULLSYSCALL;
@@ -223,18 +239,17 @@ double osm_syscall_time(unsigned int osm_iterations)
 		OSM_NULLSYSCALL;
 	}
 
-	// if (clock_gettime(CLOCK_REALTIME, &stopTime))
 	if (gettimeofday(&stopTime, NULL))
 	{
 		return DEFAULT_BAD_RESULT;
 	}
 
-	// Return difference
 	return calculateTimeDifference(startTime, stopTime) / osm_iterations;
 }
 
 
-/*  @brief Time measurement function for a simple arithmetic operation.
+/**
+ *  @brief Time measurement function for a simple arithmetic operation.
  *  	returns time in nano-seconds upon success,
  *  	and -1 upon failure.
  *  	Zero iterations number is invalid.
@@ -249,50 +264,36 @@ double osm_operation_time(unsigned int osm_iterations)
 		osm_iterations = DEFAULT_ITERATION_NUMBER;
 	}
 
-	// Set operands
-	int firstOperand = 1;
-	int secondOperand = 2;
-	int resultOperand;
-
-	// struct timespec startTime;
-	// struct timespec stopTime;
 	struct timeval startTime;
 	struct timeval stopTime;
-
-	// Start time measuring
-
 	unsigned int i;
 
-	// if (clock_gettime(CLOCK_REALTIME, &startTime))
+	// Start time measuring
 	if (gettimeofday(&startTime, NULL))
 	{
 		return DEFAULT_BAD_RESULT;
 	}
 
-	for (i = 0; i < osm_iterations; i+=10)
+	for (i = 0; i < osm_iterations; i += DEFAULT_ITER_OPERATIONS_NUMBER)
 	{
-		1+2;
-		1+2;
-		1+2;
-		1+2;
-		1+2;
-                1+2;
-                1+2;
-                1+2;
-                1+2;
-                1+2;
+		__asm__("movl $1, %eax;" "movl $2, %ebx;" "addl %ebx, %eax;");
+		__asm__("movl $1, %eax;" "movl $2, %ebx;" "addl %ebx, %eax;");
+		__asm__("movl $1, %eax;" "movl $2, %ebx;" "addl %ebx, %eax;");
+		__asm__("movl $1, %eax;" "movl $2, %ebx;" "addl %ebx, %eax;");
+		__asm__("movl $1, %eax;" "movl $2, %ebx;" "addl %ebx, %eax;");
+
+		__asm__("movl $1, %eax;" "movl $2, %ebx;" "addl %ebx, %eax;");
+		__asm__("movl $1, %eax;" "movl $2, %ebx;" "addl %ebx, %eax;");
+		__asm__("movl $1, %eax;" "movl $2, %ebx;" "addl %ebx, %eax;");
+		__asm__("movl $1, %eax;" "movl $2, %ebx;" "addl %ebx, %eax;");
+		__asm__("movl $1, %eax;" "movl $2, %ebx;" "addl %ebx, %eax;");
 	}
 
-	// if (clock_gettime(CLOCK_REALTIME, &stopTime))
 	if (gettimeofday(&stopTime, NULL))
 	{
 		return DEFAULT_BAD_RESULT;
 	}
 
-	// Dirty hack, see forum.
-	resultOperand++;
-
-	// Return difference
 	return calculateTimeDifference(startTime, stopTime) / osm_iterations;
 }
 
@@ -313,45 +314,59 @@ timeMeasurmentStructure measureTimes (unsigned int osm_iterations)
 	int successGetHostName = gethostname(results.machineName, HOST_NAME_MAX);
 	if (successGetHostName != 0)
 	{
-		// TODO wtf how to set it to null
+		// Set the hostname to be empty:
 		results.machineName[0] = '\0';
 	}
 
+	// Validate input
 	if (!isValidIterationsNumber(osm_iterations))
 	{
 		osm_iterations = DEFAULT_ITERATION_NUMBER;
 	}
 
-        double averageGettingTime = averageGettingTimeTime(osm_iterations);
-        double averageForLoopTime = averageForLoopRuntime(GIGA); 
+	// Round up the iterations if needed
+	osm_iterations = normalizeIterationsNumber(osm_iterations);
 
-	// Set iteration number
+	// Calculate utilities time
+	double avgGettingTime = avgGettimeofdayCallTime(GIGA);
+	double avgForLoopTime = avgForLoopRuntime(GIGA); 
+
+	// Save iteration number to the output struct
 	results.numberOfIterations = osm_iterations;
 
-	// Run measurements
-	results.instructionTimeNanoSecond = osm_operation_time(osm_iterations);// - averageGettingTime - averageForLoopTime;
-	results.functionTimeNanoSecond = osm_function_time(osm_iterations);// - averageGettingTime - averageForLoopTime;
-	results.trapTimeNanoSecond = osm_syscall_time(osm_iterations);// - averageGettingTime - averageForLoopTime;
+	// Run measurements and substract avg. time of (1) adding to loop counter 
+	// and (2) avg. time of gettimeofday() call
+	results.instructionTimeNanoSecond = osm_operation_time(osm_iterations) 
+										- avgGettingTime - avgForLoopTime;
+	
+	results.functionTimeNanoSecond = osm_function_time(osm_iterations) 
+									 - avgGettingTime - avgForLoopTime;
 
-	// Calculate ratios
-	if (results.functionTimeNanoSecond != DEFAULT_BAD_RESULT && results.instructionTimeNanoSecond != DEFAULT_BAD_RESULT)
+	results.trapTimeNanoSecond = osm_syscall_time(osm_iterations) 
+								 - avgGettingTime - avgForLoopTime;
+
+	// Calculate ratios, set defaults in case of an error
+	if (results.functionTimeNanoSecond != DEFAULT_BAD_RESULT && 
+		results.instructionTimeNanoSecond != DEFAULT_BAD_RESULT)
 	{
-		results.functionInstructionRatio = results.functionTimeNanoSecond / results.instructionTimeNanoSecond;
+		results.functionInstructionRatio = results.functionTimeNanoSecond / 
+										   results.instructionTimeNanoSecond;
 	}
 	else
 	{
-		results.functionInstructionRatio = DEFAULT_BAD_RESULT;	
+		results.functionInstructionRatio = DEFAULT_BAD_RESULT;
 	}
 
-	if (results.trapTimeNanoSecond != DEFAULT_BAD_RESULT && results.instructionTimeNanoSecond != DEFAULT_BAD_RESULT)
+	if (results.trapTimeNanoSecond != DEFAULT_BAD_RESULT && 
+		results.instructionTimeNanoSecond != DEFAULT_BAD_RESULT)
 	{
-		results.trapInstructionRatio = results.trapTimeNanoSecond / results.instructionTimeNanoSecond;
+		results.trapInstructionRatio = results.trapTimeNanoSecond / 
+									   results.instructionTimeNanoSecond;
 	}
 	else
 	{
-		results.trapInstructionRatio = DEFAULT_BAD_RESULT;	
+		results.trapInstructionRatio = DEFAULT_BAD_RESULT;
 	}
 
 	return results;
-
 }

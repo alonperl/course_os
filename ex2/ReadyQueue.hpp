8cac6e6cc22ee493350b3799bec8521321b257fe
griffonn <griffonn@gmail.com>
2015-04-16 22:27:37 +0300

    ex2

--- ex2/ReadyQueue.hpp
+++ ex2/ReadyQueue.hpp
@@ -9,7 +9,10 @@ public:
 	void pop();
 	Thread *top();
 	void push(Thread *thread);
-	
+	void erase(Thread *thread);
+
+	std::deque<Thread*>::iterator iterator(Priority priority);
+
 	std::deque<Thread*> red;
 	std::deque<Thread*> orange;
 	std::deque<Thread*> green;

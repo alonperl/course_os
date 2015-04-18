2040bb30dc1e9bae528ac8125910f3fbf3ce050d
griffonn <griffonn@gmail.com>
2015-04-18 14:45:29 +0300

    ex2 test tids pool

--- ex2/testTidsPool.cpp
+++ ex2/testTidsPool.cpp
@@ -7,27 +7,24 @@
 #include <list>
 #include <iostream>
 
+#include "uthreads.h"
 #include "Thread.hpp"
 #include "Scheduler.hpp"
 #include "SignalManager.hpp"
 
 int main(int argc, char const *argv[])
 {
-	Scheduler *s = Scheduler::getInstance();
-	
-	s->setQuantum(100);
+	uthreads_init(100);
+	uthreads_spawn(NULL, ORANGE);
+	uthreads_spawn(NULL, ORANGE);
+	uthreads_spawn(NULL, ORANGE);
+	uthreads_spawn(NULL, ORANGE);
+	uthreads_terminate(1);
+	printf("New thread %d\n", uthreads_spawn(NULL, ORANGE));
+	uthreads_terminate(2);
+	printf("New thread %d\n", uthreads_spawn(NULL, ORANGE));
+	uthreads_terminate(3);
+	printf("New thread %d\n", uthreads_spawn(NULL, ORANGE));
 
-	printf("Top %dn", s->getTidsPool()->top());
-
-	s->getTidsPool()->push(1);
-	s->getTidsPool()->push(2);
-
-	printf("Top %d\n", s->getTidsPool()->top());
-	s->getTidsPool()->pop();
-	printf("Pop\n");
-	printf("Top %d\n", s->getTidsPool()->top());
-	s->getTidsPool()->pop();
-	printf("Pop\n");
-	printf("Top %d\n", s->getTidsPool()->top());
-	return 0;
+	uthreads_terminate(0);
 }

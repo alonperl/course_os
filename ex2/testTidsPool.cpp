dc0a755d2249ac6e84cce7b10045c3aedb45933b
griffonn <griffonn@gmail.com>
2015-04-18 14:52:55 +0300

    ex2 test tids pool

--- ex2/testTidsPool.cpp
+++ ex2/testTidsPool.cpp
@@ -15,39 +15,56 @@
 int main(int argc, char const *argv[])
 {
 	uthread_init(100);
+	
 	printf("New thread %d\n", uthread_spawn(NULL, ORANGE));
 	printf("New thread %d\n", uthread_spawn(NULL, ORANGE));
 	printf("New thread %d\n", uthread_spawn(NULL, ORANGE));
+	
 	printf("Next free tid %d\n", Scheduler::getInstance()->getMinTid());
 
 	uthread_terminate(1);
-	printf("Next free tid %d\n", Scheduler::getInstance()->getMinTid());
-	printf("New thread %d\n", uthread_spawn(NULL, ORANGE));
-	printf("Next free tid %d\n", Scheduler::getInstance()->getMinTid());
-	uthread_terminate(2);
-	printf("Next free tid %d\n", Scheduler::getInstance()->getMinTid());
-	printf("New thread %d\n", uthread_spawn(NULL, ORANGE));
-	printf("Next free tid %d\n", Scheduler::getInstance()->getMinTid());
-	uthread_terminate(3);
-	printf("Next free tid %d\n", Scheduler::getInstance()->getMinTid());
-	printf("New thread %d\n", uthread_spawn(NULL, ORANGE));
-	printf("Next free tid %d\n", Scheduler::getInstance()->getMinTid());
-
-	uthread_terminate(1);
-	printf("Next free tid %d\n", Scheduler::getInstance()->getMinTid());
-	uthread_terminate(2);
-	printf("Next free tid %d\n", Scheduler::getInstance()->getMinTid());
-	uthread_terminate(3);
-	printf("Next free tid %d\n", Scheduler::getInstance()->getMinTid());
-	printf("New thread %d\n", uthread_spawn(NULL, ORANGE));
-	printf("Next free tid %d\n", Scheduler::getInstance()->getMinTid());
-	printf("New thread %d\n", uthread_spawn(NULL, ORANGE));
-	printf("Next free tid %d\n", Scheduler::getInstance()->getMinTid());
-	printf("New thread %d\n", uthread_spawn(NULL, ORANGE));
 	printf("Next free tid %d\n", Scheduler::getInstance()->getMinTid());
 
 	printf("New thread %d\n", uthread_spawn(NULL, ORANGE));
 	printf("Next free tid %d\n", Scheduler::getInstance()->getMinTid());
 	
+	uthread_terminate(2);
+	printf("Next free tid %d\n", Scheduler::getInstance()->getMinTid());
+	
+	printf("New thread %d\n", uthread_spawn(NULL, ORANGE));
+	printf("Next free tid %d\n", Scheduler::getInstance()->getMinTid());
+	
+	uthread_terminate(3);
+	printf("Next free tid %d\n", Scheduler::getInstance()->getMinTid());
+	
+	printf("New thread %d\n", uthread_spawn(NULL, ORANGE));
+	printf("Next free tid %d\n", Scheduler::getInstance()->getMinTid());
+
+	uthread_terminate(1);
+	printf("Next free tid %d\n", Scheduler::getInstance()->getMinTid());
+	
+	uthread_terminate(2);
+	printf("Next free tid %d\n", Scheduler::getInstance()->getMinTid());
+	
+	uthread_terminate(3);
+	printf("Next free tid %d\n", Scheduler::getInstance()->getMinTid());
+	
+	printf("New thread %d\n", uthread_spawn(NULL, ORANGE));
+	printf("Next free tid %d\n", Scheduler::getInstance()->getMinTid());
+	
+	printf("New thread %d\n", uthread_spawn(NULL, ORANGE));
+	printf("Next free tid %d\n", Scheduler::getInstance()->getMinTid());
+	
+	printf("New thread %d\n", uthread_spawn(NULL, ORANGE));
+	printf("Next free tid %d\n", Scheduler::getInstance()->getMinTid());
+
+	printf("New thread %d\n", uthread_spawn(NULL, ORANGE));
+	printf("Next free tid %d\n", Scheduler::getInstance()->getMinTid());
+
+	std::map<unsigned int, Thread*>::iterator it = Scheduler::getInstance()->getThreadsMap().begin();
+	for (; it != Scheduler::getInstance()->getThreadsMap().end(); it++)
+	{
+		printf("%d in mapn", it->second->getTid());
+	}
 	uthread_terminate(0);
 }

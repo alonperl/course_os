be0f7adc0b21376f37d3daaf5d807763464032a6
griffonn <griffonn@gmail.com>
2015-04-18 14:34:51 +0300

    ex2 test tids pool

--- ex2/testTidsPool.cpp
+++ ex2/testTidsPool.cpp
@@ -13,15 +13,15 @@
 
 int main(int argc, char const *argv[])
 {
-	printf("Top %dn", Scheduler::getInstance()->getTidsPool()->top();
+	printf("Top %dn", Scheduler::getInstance()->getTidsPool()->top());
 
 	Scheduler::getInstance()->getTidsPool()->push(1);
 	Scheduler::getInstance()->getTidsPool()->push(2);
 
-	printf("Top %d\n", Scheduler::getInstance()->getTidsPool()->top();
-	printf("Pop %d\n", Scheduler::getInstance()->getTidsPool()->pop();
-	printf("Top %d\n", Scheduler::getInstance()->getTidsPool()->top();
-	printf("Pop %d\n", Scheduler::getInstance()->getTidsPool()->pop();
-	printf("Top %d\n", Scheduler::getInstance()->getTidsPool()->top();
+	printf("Top %d\n", Scheduler::getInstance()->getTidsPool()->top());
+	printf("Pop %d\n", Scheduler::getInstance()->getTidsPool()->pop());
+	printf("Top %d\n", Scheduler::getInstance()->getTidsPool()->top());
+	printf("Pop %d\n", Scheduler::getInstance()->getTidsPool()->pop());
+	printf("Top %d\n", Scheduler::getInstance()->getTidsPool()->top());
 	return 0;
 }

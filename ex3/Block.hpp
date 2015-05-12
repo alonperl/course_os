7cd7a18481f15bb6eed1cfd942bb6c6bc67a22c0
Eran Nussinovitch <eran.nussinovitc@mail.huji.ac.il>
2015-05-12 20:10:38 +0300

    fixed some compiling errors

--- ex3/Block.hpp
+++ ex3/Block.hpp
@@ -1,38 +1,40 @@
+#include <string>
+
 class Block
 {
 public:
-	Block(arguments);
+	Block(int id, std::string& dataHash, Block* father, int height);
 	~Block();
 
 	/**
 	 * @return the block's Id
 	 */
-	int getId(void);
+	int getId();
 
 	/**
 	 * @return the block's timestamp
 	 */
-	time_t getTimestamp(void);
+	time_t getTimestamp();
 
 	/**
 	 * @return the block's height
 	 */
-	int getHeight(void);
+	int getHeight();
 
 	/**
 	 * @return the block's hashdata
 	 */
-	string getHashData(void);
+	std::string getHashData();
 
 	/**
 	 * @return the blocks father
 	 */
-	*Block getPrevBlock(void); 
+	Block *getPrevBlock(); 
 
 	private:
 		int _blockId;
 		time_t _timestamp;
 		int _height;
-		string _hashData;
+		std::string _hashData;
 		Block* _prevBlock;
 };

4909426d717666e24079ae8e9211445a0b2f5199
griffonn <griffonn@gmail.com>
2015-05-16 18:19:52 +0300

    debug Worker

--- ex3/Chain.cpp
+++ ex3/Chain.cpp
@@ -381,7 +381,7 @@ int Chain::chainSize()
 {
 	// TODO _size is updated only on actual attachment (in pushBlock)
 	// TODO and this is good, but in test:69 busy_waiting for right size stucks
-	return (isInitiated() ? _size-1 : FAIL);
+	return (isInitiated() ? _size : FAIL);
 	// return (isInitiated() ? _attached.size()-1 : FAIL);
 }
 

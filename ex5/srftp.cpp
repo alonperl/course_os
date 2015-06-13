0207b510863e1870d3e38a19a14afa46b709ffde
griffonn <griffonn@gmail.com>
2015-06-13 17:07:49 +0300

    new day new life

--- ex5/srftp.cpp
+++ ex5/srftp.cpp
@@ -276,7 +276,11 @@ void* clientHandler(void* pClient)
 			}
 			else // Save filename
 			{
-				free(filename);
+				if (filename != NULL)
+				{
+					free(filename);
+				}
+				
 				filename = (char*) malloc(sizeof(char) * (recvPacket.dataSize + 1));
 				if (filename == nullptr)
 				{
